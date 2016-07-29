25d899f9f1481fa7865f2be9ccc6f439f14fdde8
Daniel Garcia-Carrillo <garciacarrillo.daniel@yahoo.com>
2016-07-28 23:27:08 -0700

    Boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+/* eslint-env node */
+
+module.exports = {
+    entry: {
+        'dist/bundle': './src/client/index.jsx',
+        'dist/test': './test/test.js'
+    },
+    output: {
+        path: './',
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            loader: 'babel'
+        }, {
+            test: /\.css$/,
+            loader: 'style!css'
+        }]
+    }
+};
